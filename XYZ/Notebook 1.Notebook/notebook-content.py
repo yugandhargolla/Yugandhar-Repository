c7322ff6bfe799495f9a64048bc643a307b366db
yugandhargolla <yugandhargolla8@gmail.com>
2025-02-13 20:22:18 +0530

    Committing 10 items from workspace 3a6c2998-f7c3-4d14-91f4-04952156aeaf

---
+++ XYZ/Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,37 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "6f645e50-1722-4691-8809-8a90ed355ec9",
+# META       "default_lakehouse_name": "dummy",
+# META       "default_lakehouse_workspace_id": "3a6c2998-f7c3-4d14-91f4-04952156aeaf",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "6f645e50-1722-4691-8809-8a90ed355ec9"
+# META         },
+# META         {
+# META           "id": "7de44753-9aa6-4acb-bcc8-cf4c16255c11"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print("hello")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
---
+++ XYZ/Notebook 2.Notebook/notebook-content.py
@@ -0,0 +1,45 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "21dff011-fa2f-44f3-9c48-ca8dc21b7528",
+# META       "default_lakehouse_name": "Dummylake",
+# META       "default_lakehouse_workspace_id": "3a6c2998-f7c3-4d14-91f4-04952156aeaf",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "21dff011-fa2f-44f3-9c48-ca8dc21b7528"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+print("hii")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
