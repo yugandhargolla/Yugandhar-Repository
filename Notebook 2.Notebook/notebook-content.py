55831e7a976b7f0843c9e2ffd40784d962c4309a
yugandhargolla <yugandhargolla8@gmail.com>
2025-06-23 15:29:22 +0530

    Committing 28 items from workspace b8ff653e-bf3c-4afb-962e-d506aea2774e

---
+++ Notebook 2.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+
+!pip install langchain openai typing_extensions --upgrade
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+from langchain.document_loaders import TextLoader
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
